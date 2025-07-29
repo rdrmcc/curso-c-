ef9a4f20c0ea4d9f3d1a95c293254518e51f0905
rdrmcc <rdrmcc@hotmail.com>
2025-07-29 10:43:48 -0300

    Add files via upload

---
+++ Aula 01/primeiro.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main () {
+    std::cout << "Hello, World!\n"; 
+    std::cout << "Primeira aula de C++" << std::endl;
+    std::cout << "OxeTech Lab"; 
+       
+    return 0;
+
+}
